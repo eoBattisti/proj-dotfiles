727c7065d52204c81000c056d31f38278e6db743
eoBattisti <nickgabrielbd@gmail.com>
2024-08-07 22:28:10 -0300

    feat(nvim): add new todo-comments.nvim keybinds

--- .config/nvim/lua/plugins/todo_comments.lua
+++ .config/nvim/lua/plugins/todo_comments.lua
@@ -4,6 +4,17 @@ return {
     config = function()
         require("todo-comments").setup {}
 
-        vim.keymap.set("n", "<leader>t", "<cmd>TodoTelescope<CR>", { noremap = true, silent = true })
+        vim.keymap.set(
+          "n", "<leader>t", "<cmd>TodoTelescope<CR>",
+          { noremap = true, silent = true, desc = "List all comments" }
+        )
+
+        vim.keymap.set("n", "]t", function ()
+          require("todo-comments").jump_next()
+        end, { desc = "Jump to next comment"})
+
+        vim.keymap.set("n", "[t", function ()
+          require("todo-comments").jump_prev()
+        end, { desc = "Jump to previous comment"})
     end
 }
